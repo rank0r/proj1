8ce5eeebd1ce77fd24adfff18ca609678595acad
rank0r <rank0r@local.net>
2017-07-27 17:16:08 -0400

    updates

--- modules/environment.py
+++ modules/environment.py
@@ -1,7 +1,7 @@
 import os
 
 def run(**args):
-    print "[*] In environment module."
+    print "[*] In environment module"
     environ = str(os.environ)
-    mod_description = "Results for dirLister module\n\n"
+    mod_description = "Results for environment module\n\n"
     return mod_description + environ
