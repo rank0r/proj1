44866207d83ba448b98987d5400cc11f6d3277c2
rank0r <rank0r@local.net>
2017-07-27 19:51:45 -0400

    updates

--- modules/dirlister.py
+++ modules/dirlister.py
@@ -1,7 +1,14 @@
 import os 
 
-def run(**args):
+def run(*args):
     print "[*] In dirlister module"
-    files = str(os.listdir("."))
+    if len(args) == 0:
+        files = str(os.listdir("."))
+    elif len(args) == 1:
+        files = str(os.listdir(args[0]))
+    else:
+        print "[*] Too many arguments in dirlister module... Quitting"
+        sys.exit(1)
+        
     mod_description = "Results for dirLister module:\n\n"
     return mod_description + files
