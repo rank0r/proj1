d43f3ae44770592e9062f5cb012348b75ed2f3ba
rank0r <rank0r@local.net>
2017-07-27 17:31:07 -0400

    updates

--- modules/dirlister.py
+++ modules/dirlister.py
@@ -3,5 +3,5 @@ import os
 def run(**args):
     print "[*] In dirlister module"
     files = str(os.listdir("."))
-    mod_description = "Results for dirLister module\n\n"
+    mod_description = "Results for dirLister module:\n\n"
     return mod_description + files
--- modules/environment.py
+++ modules/environment.py
@@ -3,5 +3,5 @@ import os
 def run(**args):
     print "[*] In environment module"
     environ = str(os.environ)
-    mod_description = "Results for environment module\n\n"
+    mod_description = "Results for environment module:\n\n"
     return mod_description + environ
